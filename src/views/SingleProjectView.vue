5fe74af7f0581677cdad9572033cbf0f1399c83e
Cristina <cristinabosasanchez@gmail.com>
2024-03-13 18:40:12 +0000

    Remove bigger

--- src/views/SingleProjectView.vue
+++ src/views/SingleProjectView.vue
@@ -18,12 +18,10 @@
     </section>
     <section>
       <article class="container__projects--right projects">
-        <div class="projects__img" v-on:click="showBigger" @mouseenter="playSound" v-for="img in project.gallery">
+        <div class="projects__img" @mouseenter="playSound" v-for="img in project.gallery">
           <img :src="img" alt="" height="350px">
-          <img v-if="isBigger" class="bigger-image" :src="img" alt="">
-
         </div>
-        <div class="modal-overlay" v-if="isBigger" @click="hideBigger"></div>
+        
       </article>
 
     </section>
@@ -36,15 +34,7 @@ import DataProjects from '../data/projects'
 import { useRoute } from 'vue-router'
 export default defineComponent({
   setup() {
-    const isBigger = ref(false)
 
-    const showBigger = () => {
-      isBigger.value = true
-    }
-
-    const hideBigger = () => {
-      isBigger.value = false
-    }
     const route = useRoute()
     const projects = DataProjects.projects
     const id = parseInt(route.params.id)
@@ -55,9 +45,7 @@ export default defineComponent({
 
     const project = projects.find(project => project.id === id)
     return {
-      isBigger,
-      showBigger,
-      hideBigger,
+
       project,
       playSound
 
@@ -67,35 +55,11 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/sass/01-tools/querys.scss' as querys;
 .projects{
   margin-top: 5rem;
 }
 
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  /* Color de fondo semitransparente */
-  z-index: 999;
-  /* Asegúrate de que esté por encima de todo el contenido */
-}
-
-.bigger-image {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  max-width: 90%;
-  /* Ajusta el tamaño máximo según tus necesidades */
-  max-height: 90vh;
-  /* Ajusta el tamaño máximo según tus necesidades */
-  z-index: 1000;
-  /* Asegúrate de que esté por encima de la capa modal */
-}
-@use '../assets/sass/01-tools/querys.scss' as querys;
 .projects__img {
   @for $i from 1 through 6 {
     &:nth-child(#{$i}) {
