3f7d396978f92ee40486d5945ce545f287683b3a
Oriclux <cristinabosasanchez@gmail.com>
2026-08-19 15:32:48 +0100

    initialize project structure with Astro, add essential configuration files and layout components

---
+++ astro.config.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "FunnelDisplay",
+      cssVariable: "--font-funnel-display",
+      options: {
+        variants: [
+          {
+            src: ["./public/fonts/FunnelDisplay.woff"],
+            weight: "500",
+            style: "normal",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "FunnelSans",
+      cssVariable: "--font-funnel-sans",
+      options: {
+        variants: [
+          {
+            src: ["./public/fonts/FunnelSans.woff"],
+            weight: "500",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
